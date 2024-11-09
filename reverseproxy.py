aa02146edffb647ec65c24f465bf13b133525c36
shuttle-r <109020578+shuttle-r@users.noreply.github.com>
2024-11-10 01:50:11 +0800

    Update reverseproxy.py

--- reverseproxy.py
+++ reverseproxy.py
@@ -1,4 +1,12 @@
-pip install aiohttp pyngrok
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# Install required packages
+install("aiohttp")
+install("pyngrok")
 
 import os
 import asyncio
@@ -7,25 +15,25 @@ import asyncio
 os.environ.update({'LD_LIBRARY_PATH': '/usr/lib64-nvidia'})
 
 async def run_process(cmd):
-  print('>>> starting', *cmd)
-  p = await asyncio.subprocess.create_subprocess_exec(
-      *cmd,
-      stdout=asyncio.subprocess.PIPE,
-      stderr=asyncio.subprocess.PIPE,
-  )
+    print('>>> starting', *cmd)
+    p = await asyncio.subprocess.create_subprocess_exec(
+        *cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
 
-  async def pipe(lines):
-    async for line in lines:
-      print(line.strip().decode('utf-8'))
+    async def pipe(lines):
+        async for line in lines:
+            print(line.strip().decode('utf-8'))
 
-  await asyncio.gather(
-      pipe(p.stdout),
-      pipe(p.stderr),
-  )
+    await asyncio.gather(
+        pipe(p.stdout),
+        pipe(p.stderr),
+    )
 
-#register an account at ngrok.com and create an authtoken and place it here
+# Register an account at ngrok.com, create an authtoken, and place it here
 await asyncio.gather(
-    run_process(['ngrok', 'config', 'add-authtoken','your-auth-token'])
+    run_process(['ngrok', 'config', 'add-authtoken', 'your-auth-token'])
 )
 
 await asyncio.gather(
