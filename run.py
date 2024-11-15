1969b57f5adc5d691b4783a931d14c3597eda865
shuttle-r <109020578+shuttle-r@users.noreply.github.com>
2024-11-15 20:35:53 +0800

    Update run.py

--- run.py
+++ run.py
@@ -8,17 +8,17 @@ os.system('clear')
 print("Alr")
 username = "user" #@param {type:"string"}
 password = "root" #@param {type:"string"}
-os.system(f"useradd -m {username} > /dev/null 2>&1")
-os.system(f"adduser {username} sudo > /dev/null 2>&1")
-os.system(f"echo '{username}:{password}' | sudo chpasswd > /dev/null 2>&1")
-os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd > /dev/null 2>&1")
+os.system(f"useradd -m {username}")
+os.system(f"adduser {username} sudo")
+os.system(f"echo '{username}:{password}' | sudo chpasswd")
+os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
 
 Pin = 123456 #@param {type: "integer"}
 Autostart = True #@param {type: "boolean"}
 
 class CRDSetup:
     def __init__(self, user):
-        os.system("apt update -y > /dev/null 2>&1")
+        os.system("apt update -y")
         time.sleep(1)
         self.installCRD()
         time.sleep(1)
@@ -38,13 +38,13 @@ class CRDSetup:
     @staticmethod
     def installDesktopEnvironment():
         os.system("export DEBIAN_FRONTEND=noninteractive")
-        os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal > /dev/null 2>&1")
+        os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
         os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
-        os.system("apt remove --assume-yes gnome-terminal > /dev/null 2>&1")
-        os.system("apt install --assume-yes xscreensaver > /dev/null 2>&1")
-        os.system("sudo apt purge light-locker > /dev/null 2>&1")
-        os.system("sudo apt install --reinstall xfce4-screensaver > /dev/null 2>&1")
-        os.system("systemctl disable lightdm.service > /dev/null 2>&1")
+        os.system("apt remove --assume-yes gnome-terminal")
+        os.system("apt install --assume-yes xscreensaver")
+        os.system("sudo apt purge light-locker")
+        os.system("sudo apt install --reinstall xfce4-screensaver")
+        os.system("systemctl disable lightdm.service")
         print("Desk Enviro installed.")
 
     @staticmethod
@@ -71,13 +71,13 @@ Comment=Open a predefined notebook at session signin.
 X-GNOME-Autostart-enabled=true""".format(link)
             with open(f"/home/{user}/.config/autostart/colab.desktop", "w") as f:
                 f.write(colab_autostart)
-            os.system(f"chmod +x /home/{user}/.config/autostart/colab.desktop > /dev/null 2>&1")
-            os.system(f"chown {user}:{user} /home/{user}/.config > /dev/null 2>&1")
+            os.system(f"chmod +x /home/{user}/.config/autostart/colab.desktop")
+            os.system(f"chown {user}:{user} /home/{user}/.config")
             
-        os.system(f"adduser {user} chrome-remote-desktop > /dev/null 2>&1")
+        os.system(f"adduser {user} chrome-remote-desktop")
         command = f"{CRD_SSH_Code} --pin={Pin}"
-        os.system(f"su - {user} -c '{command}' > /dev/null 2>&1")
-        os.system("service chrome-remote-desktop start > /dev/null 2>&1")
+        os.system(f"su - {user} -c '{command}'")
+        os.system("service chrome-remote-desktop start")
         print("PIN: 123456")
 
 try:
